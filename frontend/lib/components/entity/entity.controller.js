7399d6a2bb970935a5b880345f30ba764a332788
Jorge Mayo Calderon <jorgemayo.pe@gmail.com>
2026-05-16 11:02:45 -0500

    Cambios

--- frontend/lib/components/entity/entity.controller.js
+++ frontend/lib/components/entity/entity.controller.js
@@ -2,7 +2,16 @@ app.controller("entityCtrl", ["$scope", function($scope) {
 
   $scope.isEntityOpen = false;
   $scope.newEntity = undefined;
+  $scope.newElement = undefined;
+  $scope.newTechnology = undefined;
   $scope.entities = [];
+  $scope.sections = [
+    {id: 1, name: "Datos Generales"},
+    {id: 2, name: "Elementos Evaluables"}
+  ];
+
+  $scope.sectionSelected = undefined;
+
   $scope.init = () => {
     console.log("Iniciando controller de Entidad");
     $scope.getEntities();
@@ -12,16 +21,45 @@ app.controller("entityCtrl", ["$scope", function($scope) {
     
   }
 
+
   $scope.toggleEntity = (entity = undefined) => {
     if ($scope.isEntityOpen) {
       $scope.isEntityOpen = false;
     } else {
+      $scope.selectSection($scope.sections[0]);
       $scope.isEntityOpen = true;
-      $scope.newEntity = {};
+      $scope.newEntity = {
+        elements: []
+      };
       if (entity) {
         $scope.newEntity = JSON.parse(JSON.stringify(entity));
+        $scope.newEntity.elements = $scope.newEntity.elements || [];
         $scope.newEntity.editing = true;
       }
+    }
+  }
+
+  $scope.selectSection = (section) => {
+    $scope.sectionSelected = section;
+  }
+
+  $scope.toggleTechnology = (technology, drop) => {
+    if (drop) {
+      $scope.newElement.technologies.splice($scope.newElement.technologies.indexOf(technology), 1);
+    } else {
+      console.log(technology);
+      if ($scope.newElement.technologies.includes(technology)) {
+        Swal.fire({
+          icon: 'warning',
+          text: 'Esta tecnologia ya se encuentra agregada',
+        });
+        return;
+      }
+      $scope.newElement.technologies.push(technology);
+      var el = angular.element(document.querySelector('#technology'));
+      el.val("");
+      technology = undefined;
+      $scope.newTechnology = undefined;
     }
   }
 
@@ -32,6 +70,30 @@ app.controller("entityCtrl", ["$scope", function($scope) {
       text: 'Se grabo correctamente la entidad'
     });
     $scope.newEntity = undefined;
+  }
+
+  $scope.toggleElement = (element = undefined) => {
+    if ($scope.newElement) {
+      $scope.newElement = undefined;
+    } else {
+      // Editar Elemento
+      if (element) {
+        $scope.newElement = JSON.parse(JSON.stringify(element));
+      } else {
+        $scope.newElement = {
+          technologies: []
+        };
+      }
+    }
+  }
+
+  $scope.saveElement = () => {
+    $scope.newEntity.elements.push($scope.newElement);
+    $scope.newElement = undefined;
+  }
+
+  $scope.saveEntity = () => {
+    console.log($scope.newEntity);
   }
 
 }])
