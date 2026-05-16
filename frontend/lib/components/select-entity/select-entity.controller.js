79cef0da5cf38dc688111072a5a421e270633cf0
Jorge Mayo Calderon <jorgemayo.pe@gmail.com>
2026-05-16 01:37:43 -0500

    Agregando componentes iniciales

---
+++ frontend/lib/components/select-entity/select-entity.controller.js
@@ -0,0 +1,5 @@
+app.controller('selectEntityCtrl', ["$scope", "mainService", "$timeout", function($scope, mainService, $timeout)  {
+  $scope.init = () => {
+    console.log("Iniciando controlador");
+  }
+}]);
---
+++ frontend/lib/components/select-role/select-role.controller.js
@@ -0,0 +1,5 @@
+app.controller('selectRoleCtrl', ["$scope", "mainService", "$timeout", function($scope, mainService, $timeout)  {
+  $scope.init = () => {
+    console.log("Iniciando controlador");
+  }
+}]);
